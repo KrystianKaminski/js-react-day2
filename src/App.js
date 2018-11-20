947e3f8d0b65314f51dbaee2e32df88e3a419644
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-20 14:07:06 +0100

    React router install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
 import Route1 from './Route1'
 import Route2 from './Route2'
 import Route3 from './Route3'
 
-const names = ['Ala', 'Ola', 'Ela']
-const namesList = names.map(name => <li key={name}>{name}</li>)
 
 // 1. Display all names
 // 2. Display all names in ul / li list
