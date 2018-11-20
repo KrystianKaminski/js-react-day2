311d8e955151a722096118c69d44b396e4de278e
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-20 11:23:24 +0100

    First default component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>Hello</div>
-      );
-  }
-}
+
+const App = (props) => (
+  <div>Hello world!</div>
+)
 
 export default App;
