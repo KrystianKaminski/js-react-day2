23a317909b60ef3fc17540e3d9895718b60dae67
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-20 11:42:09 +0100

    Task 1 - rendering ul with li elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const names = ['Ala', 'Ola', 'Ela']
+
+// 1. Display all names
+// 2. Display all names in ul / li list
+
 
 const App = (props) => (
-  <div>Hello world!</div>
+ <div>
+    <h1>{names}</h1>
+    <ul>
+      {names.map(name => (
+        <li key={name.toString()}>{name}</li>
+      ))}
+    </ul>
+  </div>
 )
-
 export default App;
